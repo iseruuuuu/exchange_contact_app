21dc2ce4a2ee07a3fb844ad651b4f33793add500
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-24 13:05:25 +0900

    add: homeのcontrollerとstateを繋げた。

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
-import 'package:exchange_contact_app/setting/setting_screen.dart';
 import 'package:flutter/material.dart';
 import '../component/cell_item.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'home_screen_state.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,43 +15,48 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          '',
-          style: TextStyle(
-            fontSize: 20,
-          ),
-        ),
-        leading: IconButton(
-          onPressed: () {
+    return StateNotifierProvider<HomeScreenController, HomeScreenState>(
+      create: (context) => HomeScreenController(context: context),
+      builder: (context, _) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              '',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            leading: IconButton(
+              onPressed: () {
 
-          },
-          icon: Icon(Icons.settings),
-        ),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 2,
-          // Generate 100 widgets that display their index in the List.
-          children: [
-            CellItem(name: 'LINE',
-                color: Colors.green
+              },
+              icon: Icon(Icons.settings),
             ),
-            CellItem(
-              name: 'Twiter',
-              color: Colors.lightBlueAccent,
+          ),
+          body: Container(
+            width: double.infinity,
+            child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 2,
+              // Generate 100 widgets that display their index in the List.
+              children: [
+                CellItem(name: 'LINE',
+                    color: Colors.green
+                ),
+                CellItem(
+                  name: 'Twiter',
+                  color: Colors.lightBlueAccent,
+                ),
+                CellItem(
+                  name: 'Instagram',
+                  color: Colors.purple,
+                ),
+              ],
             ),
-            CellItem(
-              name: 'Instagram',
-              color: Colors.purple,
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
