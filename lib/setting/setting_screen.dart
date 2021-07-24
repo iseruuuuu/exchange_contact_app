81eb242e0b98dbbc6839209c73bfd464b1dd2a99
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-24 12:52:15 +0900

    add: controllerとstateを結んだ

--- lib/setting/setting_screen.dart
+++ lib/setting/setting_screen.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'setting_screen_state.dart';
+
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Center(child: Text('LINEを登録'),),
-            Text('Twiterを登録'),
-            Text('Instagramを登録'),
-            Text('FaceBookを登録'),
-          ],
-        ),
-      ),
+    return StateNotifierProvider<SettingScreenController, SettingScreenState>(
+      create: (context) => SettingScreenController(context: context),
+      builder: (context, _) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Center(child: Text('LINEを登録'),),
+                Text('Twiterを登録'),
+                Text('Instagramを登録'),
+                Text('FaceBookを登録'),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
