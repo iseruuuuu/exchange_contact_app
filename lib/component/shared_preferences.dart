045fa252fbbea1aec41523c78a923ada9e6284d7
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-24 23:55:14 +0900

    add: SharePreferenceのファイルを追加

---
+++ lib/component/shared_preferences.dart
@@ -0,0 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:enum_to_string/enum_to_string.dart';
+
+enum PreferenceKey {
+  //TODO　後で追加する。
+  LineID,
+  TwiterID,
+  InstagramID
+}
+
+class SharePreference {
+  final preference = SharedPreferences.getInstance();
+
+  Future<String> getString(PreferenceKey key) async {
+    final pref = await preference;
+    final value = pref.getString(EnumToString.convertToString(key)) ?? '';
+    return value;
+  }
+
+  Future<void> setString(PreferenceKey key, String value) async{
+    final pref = await preference;
+    await pref.setString(EnumToString.convertToString(key), value);
+  }
+}
