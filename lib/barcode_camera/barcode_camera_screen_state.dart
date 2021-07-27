91b3fdb98037a23845bff5c108a330746d1bdd57
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-27 23:06:58 +0900

    add: バーコードを読み込む画面を追加

---
+++ lib/barcode_camera/barcode_camera_screen_state.dart
@@ -0,0 +1,106 @@
+import 'package:exchange_contact_app/component/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+part 'barcode_camera_screen_state.freezed.dart';
+
+@freezed
+abstract class BarcodeCameraScreenState with _$BarcodeCameraScreenState {
+  const factory BarcodeCameraScreenState({
+    @Default('') String LineID,
+    @Default('') String TwiterID,
+    @Default('') String InstagramID,
+    @Default('') String FaceBookID,
+  }) = _BarcodeCameraScreenState;
+
+}
+class BarcodeCameraScreenController extends StateNotifier<BarcodeCameraScreenState> with LocatorMixin {
+  BarcodeCameraScreenController({
+    required this.context,
+    required this.sharePreference,
+  }) : super (const BarcodeCameraScreenState());
+
+  final BuildContext context;
+  final SharePreference sharePreference;
+
+  @override
+  void initState() {
+    sharePreference.getString(PreferenceKey.LineID).then((lineID) {
+      if (lineID.isEmpty) return;
+      state = state.copyWith(
+        LineID: lineID,
+      );
+    });
+
+    sharePreference.getString(PreferenceKey.TwiterID).then((TwiterID) {
+      if (TwiterID.isEmpty) return;
+      state = state.copyWith(
+        TwiterID: TwiterID,
+      );
+    });
+
+    sharePreference.getString(PreferenceKey.InstagramID).then((InstagramID) {
+      if (InstagramID.isEmpty) return;
+      state = state.copyWith(
+        InstagramID: InstagramID,
+      );
+    });
+
+    sharePreference.getString(PreferenceKey.FaceBookID).then((FaceBookID) {
+      if (FaceBookID.isEmpty) return;
+      state = state.copyWith(
+        FaceBookID: FaceBookID,
+      );
+    });
+  }
+
+  void onTapStore() {
+    //TODO ラインのURLを保存
+    final line = state.LineID;
+    sharePreference.setString(PreferenceKey.LineID, line);
+
+    //TODO TwiterのURLを保存
+    final twiter = state.TwiterID;
+    sharePreference.setString(PreferenceKey.TwiterID, twiter);
+
+    //TODO instagramのURLを保存
+    final instagram = state.InstagramID;
+    sharePreference.setString(PreferenceKey.InstagramID, instagram);
+
+    //TODO FaceBookのURLを保存
+    final facebook = state.FaceBookID;
+    sharePreference.setString(PreferenceKey.FaceBookID, facebook);
+
+    //TODO 前の画面に戻る
+    Navigator.of(context).pop();
+  }
+
+  void onTapBack() {
+    Navigator.of(context).pop();
+  }
+
+  void onChangeLine(String text) {
+    state = state.copyWith(
+      LineID: text,
+    );
+  }
+  void onChangeTwiter(String text) {
+    state = state.copyWith(
+      TwiterID: text,
+    );
+  }
+
+  void onChangeInstagram(String text) {
+    state = state.copyWith(
+      InstagramID: text,
+    );
+  }
+
+  void onChangeFaceBook(String text) {
+    state = state.copyWith(
+      FaceBookID: text,
+    );
+  }
+
+}
---
+++ lib/barcode_camera/barcode_camera_screen_state.freezed.dart
@@ -0,0 +1,223 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'barcode_camera_screen_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$BarcodeCameraScreenStateTearOff {
+  const _$BarcodeCameraScreenStateTearOff();
+
+  _BarcodeCameraScreenState call(
+      {String LineID = '',
+      String TwiterID = '',
+      String InstagramID = '',
+      String FaceBookID = ''}) {
+    return _BarcodeCameraScreenState(
+      LineID: LineID,
+      TwiterID: TwiterID,
+      InstagramID: InstagramID,
+      FaceBookID: FaceBookID,
+    );
+  }
+}
+
+/// @nodoc
+const $BarcodeCameraScreenState = _$BarcodeCameraScreenStateTearOff();
+
+/// @nodoc
+mixin _$BarcodeCameraScreenState {
+  String get LineID => throw _privateConstructorUsedError;
+  String get TwiterID => throw _privateConstructorUsedError;
+  String get InstagramID => throw _privateConstructorUsedError;
+  String get FaceBookID => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $BarcodeCameraScreenStateCopyWith<BarcodeCameraScreenState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $BarcodeCameraScreenStateCopyWith<$Res> {
+  factory $BarcodeCameraScreenStateCopyWith(BarcodeCameraScreenState value,
+          $Res Function(BarcodeCameraScreenState) then) =
+      _$BarcodeCameraScreenStateCopyWithImpl<$Res>;
+  $Res call(
+      {String LineID, String TwiterID, String InstagramID, String FaceBookID});
+}
+
+/// @nodoc
+class _$BarcodeCameraScreenStateCopyWithImpl<$Res>
+    implements $BarcodeCameraScreenStateCopyWith<$Res> {
+  _$BarcodeCameraScreenStateCopyWithImpl(this._value, this._then);
+
+  final BarcodeCameraScreenState _value;
+  // ignore: unused_field
+  final $Res Function(BarcodeCameraScreenState) _then;
+
+  @override
+  $Res call({
+    Object? LineID = freezed,
+    Object? TwiterID = freezed,
+    Object? InstagramID = freezed,
+    Object? FaceBookID = freezed,
+  }) {
+    return _then(_value.copyWith(
+      LineID: LineID == freezed
+          ? _value.LineID
+          : LineID // ignore: cast_nullable_to_non_nullable
+              as String,
+      TwiterID: TwiterID == freezed
+          ? _value.TwiterID
+          : TwiterID // ignore: cast_nullable_to_non_nullable
+              as String,
+      InstagramID: InstagramID == freezed
+          ? _value.InstagramID
+          : InstagramID // ignore: cast_nullable_to_non_nullable
+              as String,
+      FaceBookID: FaceBookID == freezed
+          ? _value.FaceBookID
+          : FaceBookID // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$BarcodeCameraScreenStateCopyWith<$Res>
+    implements $BarcodeCameraScreenStateCopyWith<$Res> {
+  factory _$BarcodeCameraScreenStateCopyWith(_BarcodeCameraScreenState value,
+          $Res Function(_BarcodeCameraScreenState) then) =
+      __$BarcodeCameraScreenStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {String LineID, String TwiterID, String InstagramID, String FaceBookID});
+}
+
+/// @nodoc
+class __$BarcodeCameraScreenStateCopyWithImpl<$Res>
+    extends _$BarcodeCameraScreenStateCopyWithImpl<$Res>
+    implements _$BarcodeCameraScreenStateCopyWith<$Res> {
+  __$BarcodeCameraScreenStateCopyWithImpl(_BarcodeCameraScreenState _value,
+      $Res Function(_BarcodeCameraScreenState) _then)
+      : super(_value, (v) => _then(v as _BarcodeCameraScreenState));
+
+  @override
+  _BarcodeCameraScreenState get _value =>
+      super._value as _BarcodeCameraScreenState;
+
+  @override
+  $Res call({
+    Object? LineID = freezed,
+    Object? TwiterID = freezed,
+    Object? InstagramID = freezed,
+    Object? FaceBookID = freezed,
+  }) {
+    return _then(_BarcodeCameraScreenState(
+      LineID: LineID == freezed
+          ? _value.LineID
+          : LineID // ignore: cast_nullable_to_non_nullable
+              as String,
+      TwiterID: TwiterID == freezed
+          ? _value.TwiterID
+          : TwiterID // ignore: cast_nullable_to_non_nullable
+              as String,
+      InstagramID: InstagramID == freezed
+          ? _value.InstagramID
+          : InstagramID // ignore: cast_nullable_to_non_nullable
+              as String,
+      FaceBookID: FaceBookID == freezed
+          ? _value.FaceBookID
+          : FaceBookID // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_BarcodeCameraScreenState implements _BarcodeCameraScreenState {
+  const _$_BarcodeCameraScreenState(
+      {this.LineID = '',
+      this.TwiterID = '',
+      this.InstagramID = '',
+      this.FaceBookID = ''});
+
+  @JsonKey(defaultValue: '')
+  @override
+  final String LineID;
+  @JsonKey(defaultValue: '')
+  @override
+  final String TwiterID;
+  @JsonKey(defaultValue: '')
+  @override
+  final String InstagramID;
+  @JsonKey(defaultValue: '')
+  @override
+  final String FaceBookID;
+
+  @override
+  String toString() {
+    return 'BarcodeCameraScreenState(LineID: $LineID, TwiterID: $TwiterID, InstagramID: $InstagramID, FaceBookID: $FaceBookID)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _BarcodeCameraScreenState &&
+            (identical(other.LineID, LineID) ||
+                const DeepCollectionEquality().equals(other.LineID, LineID)) &&
+            (identical(other.TwiterID, TwiterID) ||
+                const DeepCollectionEquality()
+                    .equals(other.TwiterID, TwiterID)) &&
+            (identical(other.InstagramID, InstagramID) ||
+                const DeepCollectionEquality()
+                    .equals(other.InstagramID, InstagramID)) &&
+            (identical(other.FaceBookID, FaceBookID) ||
+                const DeepCollectionEquality()
+                    .equals(other.FaceBookID, FaceBookID)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(LineID) ^
+      const DeepCollectionEquality().hash(TwiterID) ^
+      const DeepCollectionEquality().hash(InstagramID) ^
+      const DeepCollectionEquality().hash(FaceBookID);
+
+  @JsonKey(ignore: true)
+  @override
+  _$BarcodeCameraScreenStateCopyWith<_BarcodeCameraScreenState> get copyWith =>
+      __$BarcodeCameraScreenStateCopyWithImpl<_BarcodeCameraScreenState>(
+          this, _$identity);
+}
+
+abstract class _BarcodeCameraScreenState implements BarcodeCameraScreenState {
+  const factory _BarcodeCameraScreenState(
+      {String LineID,
+      String TwiterID,
+      String InstagramID,
+      String FaceBookID}) = _$_BarcodeCameraScreenState;
+
+  @override
+  String get LineID => throw _privateConstructorUsedError;
+  @override
+  String get TwiterID => throw _privateConstructorUsedError;
+  @override
+  String get InstagramID => throw _privateConstructorUsedError;
+  @override
+  String get FaceBookID => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$BarcodeCameraScreenStateCopyWith<_BarcodeCameraScreenState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
