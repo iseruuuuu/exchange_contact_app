30b395e89039c5c52cf70e29818bf87d55e35411
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-27 22:35:02 +0900

    add: メソッドを繋げた

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:exchange_contact_app/component/cell_item.dart';
 import 'package:exchange_contact_app/component/shared_preferences.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'home_screen_state.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -44,6 +46,94 @@ class _HomeState extends State<Home> {
             child: GridView.count(
               crossAxisCount: 2,
               children: [
+
+                const Text(
+                  'LINE',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(20),
+                  child: BarcodeWidget(
+                    barcode: Barcode.code128(),
+                    //後で追加
+                    data: 'int',
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 110,
+                    drawText: false,
+                  ),
+                ),
+
+                const Text(
+                  'LINE',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(20),
+                  child: BarcodeWidget(
+                    barcode: Barcode.code128(),
+                    //後で追加
+                    data: 'int',
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 110,
+                    drawText: false,
+                  ),
+                ),
+
+                const Text(
+                  'LINE',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(20),
+                  child: BarcodeWidget(
+                    barcode: Barcode.code128(),
+                    //後で追加
+                    data: 'int',
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 110,
+                    drawText: false,
+                  ),
+                ),
+
+                const Text(
+                  'LINE',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.green,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(20),
+                  child: BarcodeWidget(
+                    barcode: Barcode.code128(),
+                    //後で追加
+                    data: 'int',
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 110,
+                    drawText: false,
+                  ),
+                ),
+
+
+
+
+
+
+
+                /*
                 CellItem(name: 'LINE',
                     color: Colors.green
                 ),
@@ -55,6 +145,8 @@ class _HomeState extends State<Home> {
                   name: 'Instagram',
                   color: Colors.purple,
                 ),
+
+                 */
               ],
             ),
           ),
