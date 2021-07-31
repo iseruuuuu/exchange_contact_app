ee76c452378e6f04476d0cdac20a12de4235d562
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-31 11:22:31 +0900

    add: Instagramの内容を追加

--- lib/tutorial/instagram_tutorial_screen.dart
+++ lib/tutorial/instagram_tutorial_screen.dart
@@ -22,24 +22,16 @@ class InstagramTutorialScreen extends StatelessWidget {
       color: const Color(0xFF95cedd),
       //TODO 後で作る。
       imageAssetPath: 'assets/2.png',
-      title: '簡単に連絡先を交換！',
-      body: 'あらかじめアドレスを登録することで、登録してもらうためのQRコードを発行されます。',
+      title: 'URLの取得方法①',
+      body: 'Instagramを開いて\n'
+          'プロフィールを開いてください。',
       doAnimateImage: true,
     ),
     PageModel(
       color: Colors.blueAccent,
       imageAssetPath: 'assets/1.png',
-      title: '自分の連絡先の渡し方',
-      body: 'カメラorこのアプリを開いてもらってください。\n'
-          'そして、あなたのQRコードをカメラで読み込ませてください。',
-      doAnimateImage: true,
-    ),
-    PageModel(
-      color: Colors.green,
-      imageAssetPath: 'assets/1.png',
-      title: 'アカウントの変更方法',
-      body: '設定画面にていつでも変更可能です。\n'
-          '⚙︎を押して変更を行ってください。',
+      title: 'URLの取得方法②',
+      body: 'ユーザーネームを入力してください',
       doAnimateImage: true,
     ),
     PageModel.withChild(
