28870a5b4213bc8431490d3ee00f07cd41378255
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-31 11:34:04 +0900

    add:Facebookに内容を記載

--- lib/tutorial/facebook_tutorial_screen.dart
+++ lib/tutorial/facebook_tutorial_screen.dart
@@ -22,24 +22,24 @@ class FacebookTutorialScreen extends StatelessWidget {
       color: const Color(0xFF95cedd),
       //TODO 後で作る。
       imageAssetPath: 'assets/2.png',
-      title: '簡単に連絡先を交換！',
-      body: 'あらかじめアドレスを登録することで、登録してもらうためのQRコードを発行されます。',
+      title: 'URLの取得方法①',
+      body: 'メニューから「自分のプロフィールをみる\n'
+          'を開いてください。',
       doAnimateImage: true,
     ),
     PageModel(
       color: Colors.blueAccent,
       imageAssetPath: 'assets/1.png',
-      title: '自分の連絡先の渡し方',
-      body: 'カメラorこのアプリを開いてもらってください。\n'
-          'そして、あなたのQRコードをカメラで読み込ませてください。',
+      title: 'URLの取得方法②',
+      body: '「...」を押してください',
       doAnimateImage: true,
     ),
     PageModel(
       color: Colors.green,
       imageAssetPath: 'assets/1.png',
       title: 'アカウントの変更方法',
-      body: '設定画面にていつでも変更可能です。\n'
-          '⚙︎を押して変更を行ってください。',
+      body: '「プロフィールのリンク」から\n'
+          'リンクをコピーして貼り付けてください。',
       doAnimateImage: true,
     ),
     PageModel.withChild(
