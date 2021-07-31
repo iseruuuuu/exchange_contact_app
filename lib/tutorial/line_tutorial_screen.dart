121c874c143813aada473eaf61a3144678b7dd8a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-07-31 11:03:42 +0900

    add: LINEの登録方法を記載（内容のみ）

--- lib/tutorial/line_tutorial_screen.dart
+++ lib/tutorial/line_tutorial_screen.dart
@@ -22,24 +22,36 @@ class LineTutorialScreen extends StatelessWidget {
       color: const Color(0xFF95cedd),
       //TODO 後で作る。
       imageAssetPath: 'assets/2.png',
-      title: '簡単に連絡先を交換！',
-      body: 'あらかじめアドレスを登録することで、登録してもらうためのQRコードを発行されます。',
+      title: 'URLの取得方法①',
+      body: 'LINEのホームの追加画面から\n'
+          '「QRコード」を選択します。',
       doAnimateImage: true,
     ),
     PageModel(
       color: Colors.blueAccent,
       imageAssetPath: 'assets/1.png',
-      title: '自分の連絡先の渡し方',
-      body: 'カメラorこのアプリを開いてもらってください。\n'
-          'そして、あなたのQRコードをカメラで読み込ませてください。',
+      title: 'URLの取得方法②',
+      body: 'QRコードを読み込む画面から\n'
+          '「マイQRコード」を押します。\n'
+      //TODO　アイコンを後で探す。
+          '',
       doAnimateImage: true,
     ),
     PageModel(
       color: Colors.green,
       imageAssetPath: 'assets/1.png',
-      title: 'アカウントの変更方法',
-      body: '設定画面にていつでも変更可能です。\n'
-          '⚙︎を押して変更を行ってください。',
+      title: 'URLの取得方法③',
+      body: '「他のアプリ」を押してください\n'
+          '「コピー」を押してください',
+      doAnimateImage: true,
+    ),
+
+    PageModel(
+      color: Colors.green,
+      imageAssetPath: 'assets/1.png',
+      title: 'URLの取得方法④',
+      body: 'メモ帳などを開いてください。\n'
+          'URLを貼り付けてください。',
       doAnimateImage: true,
     ),
     PageModel.withChild(
