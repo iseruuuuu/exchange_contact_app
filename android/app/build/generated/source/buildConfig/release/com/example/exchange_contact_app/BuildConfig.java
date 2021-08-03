14da4f341d408c8f04d5e6f4de3f8ae7bd5c1c45
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-03 09:30:55 +0900

    add: バージョンの変更

--- android/app/build/generated/source/buildConfig/release/com/example/exchange_contact_app/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/example/exchange_contact_app/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.exchange_contact_app";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 2;
+  public static final int VERSION_CODE = 4;
   public static final String VERSION_NAME = "1.0.0";
 }
